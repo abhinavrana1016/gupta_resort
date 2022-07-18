498e2b6a18b3dcf05c108baf4a89cc0259a0622d
Varun Rana <abhinav.rana1016@gmail.com>
2022-07-18 21:14:49 +0530

    header change

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Navbar.css"
 const Navbar = () => {
+    const [isMobile,setIsMObile] = useState(false)
   return (
     <>
     <nav className='navbar'>
         <h3 className='logo'>Gupta resort</h3>
         <ul className='nav-links'>
             <Link to="/" className='home'>
-                <li>home</li>
+                <li>Home</li>
             </Link>
             <Link to="/about" className='about'>
                 <li>About</li>
@@ -20,6 +21,9 @@ const Navbar = () => {
                 <li>Restaurent</li>
             </Link>
         </ul>
+        <button className='mobile-menu-icon'>
+            {isMobile ? ( <i className='fas fa-times'></i>):(<i className='fas fa-bars'></i>)}
+        </button>
     </nav>
     </>
   )
